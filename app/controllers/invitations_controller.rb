03133cdd285d6e8cd451323c1df70e174cecf60c
Anja Greifenberg <anja.gberg@gmx.de>
2022-04-01 11:34:10 +0200

    added links to cycle list

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,10 +1,11 @@
 class InvitationsController < ApplicationController
 
   def new
+    @cycle = Cycle.find(params[:id])
     @invitation = Invitation.new
-    respond_to do |format|
-      format.html
-      format.js
-    end
+    # respond_to do |format|
+    #   format.html
+    #   format.js
+    # end
   end
 end
