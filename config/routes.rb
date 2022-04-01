f51b8683cb4e85e45722dfb332da2cbfef7180b1
Anja Greifenberg <anja.gberg@gmx.de>
2022-04-01 11:26:42 +0200

    nested routes to get cycle id for which to create invitation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'cycles#index'
-  resources :invitations, only: [:new, :create]
+  resources :cycles, only: [:index] do
+    resources :invitations, only: [:new, :create]
+  end
 end
