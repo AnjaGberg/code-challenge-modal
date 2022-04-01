86d58a5881338b7eb9befda5f72823e9a170420f
Anja Greifenberg <anja.gberg@gmx.de>
2022-04-01 12:56:01 +0200

    added format.js to def new and changed :id to :cycle_id, added data to link_to in the cycle list

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,11 +1,11 @@
 class InvitationsController < ApplicationController
 
   def new
-    @cycle = Cycle.find(params[:id])
+    @cycle = Cycle.find(params[:cycle_id])
     @invitation = Invitation.new
-    # respond_to do |format|
-    #   format.html
-    #   format.js
-    # end
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 end
