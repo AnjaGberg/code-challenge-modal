5daabc8e85954978b993b91a4e05d96e77181b68
Anja Greifenberg <anja.gberg@gmx.de>
2022-04-04 14:58:18 +0200

    some tidying

--- app/javascript/controllers/open_modal_controller.js
+++ app/javascript/controllers/open_modal_controller.js
@@ -3,13 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["modal"]
 
-  connect() {
-    console.log("open-modal controller connected");
-  }
-
   show(event){
-    console.log(event.currentTarget);
-
     let modalController = this.application.getControllerForElementAndIdentifier(
       this.modalTarget,
       "modal"
